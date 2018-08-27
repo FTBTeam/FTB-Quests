c8d512bea5c4126de4bd5c210ae6aecb41f4a402
LatvianModder <latvianmodder@gmail.com>
2018-08-27 22:18:37 +0300

    Added Quest Book item

---
+++ src/main/java/com/feed_the_beast/ftbquests/item/ItemQuestBook.java
@@ -0,0 +1,35 @@
+package com.feed_the_beast.ftbquests.item;
+
+import com.feed_the_beast.ftbquests.client.ClientQuestFile;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+/**
+ * @author LatvianModder
+ */
+public class ItemQuestBook extends Item
+{
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
+	{
+		if (world.isRemote)
+		{
+			openGui();
+		}
+
+		return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+	}
+
+	private void openGui()
+	{
+		if (ClientQuestFile.exists())
+		{
+			ClientQuestFile.INSTANCE.openQuestGui();
+		}
+	}
+}
