752b9846cf29638fbd844cc4ef5d2b8098084ba5
LatvianModder <latvianmodder@gmail.com>
2018-09-03 02:07:53 +0300

    Added true/false completions for editing mode command

--- src/main/java/com/feed_the_beast/ftbquests/command/CommandEditingMode.java
+++ src/main/java/com/feed_the_beast/ftbquests/command/CommandEditingMode.java
@@ -6,7 +6,12 @@ import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author LatvianModder
@@ -35,6 +40,17 @@ public class CommandEditingMode extends CommandBase
 	public boolean checkPermission(MinecraftServer server, ICommandSender sender)
 	{
 		return server.isSinglePlayer() || super.checkPermission(server, sender);
+	}
+
+	@Override
+	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos)
+	{
+		if (args.length == 1)
+		{
+			return getListOfStringsMatchingLastWord(args, "true", "false");
+		}
+
+		return Collections.emptyList();
 	}
 
 	@Override
