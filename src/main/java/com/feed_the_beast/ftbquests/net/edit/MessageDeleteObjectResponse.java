4fd89b9b6ef1b048719f4871e0137f230ca89010
LatvianModder <latvianmodder@gmail.com>
2018-09-03 01:44:48 +0300

    Fixed cached data not updating after object deletion

--- src/main/java/com/feed_the_beast/ftbquests/net/edit/MessageDeleteObjectResponse.java
+++ src/main/java/com/feed_the_beast/ftbquests/net/edit/MessageDeleteObjectResponse.java
@@ -55,6 +55,7 @@ public class MessageDeleteObjectResponse extends MessageToClient
 			{
 				object.deleteChildren();
 				object.deleteSelf();
+				ClientQuestFile.INSTANCE.clearCachedData();
 				ClientQuestFile.INSTANCE.refreshGui(ClientQuestFile.INSTANCE);
 			}
 		}
