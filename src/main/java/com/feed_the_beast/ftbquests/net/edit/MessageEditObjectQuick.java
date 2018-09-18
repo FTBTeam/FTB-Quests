993a590d6297be0b74556bd05f5075098ffc95b7
LatvianModder <latvianmodder@gmail.com>
2018-09-18 19:33:33 +0300

    Moved some config options to context menu as shortcut

--- src/main/java/com/feed_the_beast/ftbquests/net/edit/MessageEditObjectQuick.java
+++ src/main/java/com/feed_the_beast/ftbquests/net/edit/MessageEditObjectQuick.java
@@ -1,39 +1,37 @@
 package com.feed_the_beast.ftbquests.net.edit;
 
 import com.feed_the_beast.ftblib.lib.config.ConfigGroup;
-import com.feed_the_beast.ftblib.lib.config.IConfigCallback;
-import com.feed_the_beast.ftblib.lib.data.FTBLibAPI;
+import com.feed_the_beast.ftblib.lib.config.ConfigValueInstance;
+import com.feed_the_beast.ftblib.lib.config.IIteratingConfig;
 import com.feed_the_beast.ftblib.lib.io.DataIn;
 import com.feed_the_beast.ftblib.lib.io.DataOut;
 import com.feed_the_beast.ftblib.lib.net.MessageToServer;
 import com.feed_the_beast.ftblib.lib.net.NetworkWrapper;
-import com.feed_the_beast.ftblib.lib.util.StringUtils;
 import com.feed_the_beast.ftbquests.FTBQuests;
 import com.feed_the_beast.ftbquests.quest.QuestObject;
 import com.feed_the_beast.ftbquests.quest.ServerQuestFile;
 import com.feed_the_beast.ftbquests.quest.tasks.QuestTask;
 import com.feed_the_beast.ftbquests.quest.tasks.QuestTaskType;
-import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.util.text.TextFormatting;
 
 /**
  * @author LatvianModder
  */
-public class MessageEditObject extends MessageToServer implements IConfigCallback
+public class MessageEditObjectQuick extends MessageToServer
 {
 	private String id;
+	private String configId;
+	private boolean next;
 
-	public MessageEditObject()
+	public MessageEditObjectQuick()
 	{
 	}
 
-	public MessageEditObject(String i)
+	public MessageEditObjectQuick(String i, String c, boolean n)
 	{
 		id = i;
+		configId = c;
+		next = n;
 	}
 
 	@Override
@@ -46,12 +44,16 @@ public class MessageEditObject extends MessageToServer implements IConfigCallbac
 	public void writeData(DataOut data)
 	{
 		data.writeString(id);
+		data.writeString(configId);
+		data.writeBoolean(next);
 	}
 
 	@Override
 	public void readData(DataIn data)
 	{
 		id = data.readString();
+		configId = data.readString();
+		next = data.readBoolean();
 	}
 
 	@Override
@@ -63,43 +65,28 @@ public class MessageEditObject extends MessageToServer implements IConfigCallbac
 
 			if (object != null)
 			{
-				ITextComponent idc = new TextComponentString(" " + object.getID());
-				idc.getStyle().setColor(TextFormatting.DARK_GRAY);
-				idc.getStyle().setBold(false);
-
 				ConfigGroup group = ConfigGroup.newGroup(FTBQuests.MOD_ID);
-				group.setDisplayName(new TextComponentTranslation(object.getObjectType().getTranslationKey()).appendSibling(idc));
 				ConfigGroup group1 = group.getGroup(object.getObjectType().getName());
-				group1.setDisplayName(object.getDisplayName().appendSibling(StringUtils.color(new TextComponentString(" " + object.getID()), TextFormatting.DARK_GRAY)));
 				ConfigGroup g = group1;
 
 				if (object instanceof QuestTask)
 				{
 					QuestTaskType type = QuestTaskType.getType(object.getClass());
 					g = group1.getGroup(type.getRegistryName().getNamespace()).getGroup(type.getRegistryName().getPath());
-					group.setDisplayName(type.getDisplayName().appendSibling(idc));
 				}
 
 				object.getConfig(g);
 				object.getExtraConfig(g);
-				FTBLibAPI.editServerConfig(player, group, this);
+
+				ConfigValueInstance inst = g.getValueInstance(configId);
+
+				if (inst != null && inst.getValue() instanceof IIteratingConfig)
+				{
+					((IIteratingConfig) inst.getValue()).iterate(inst, next);
+					new MessageEditObjectQuickResponse(id, configId, next).sendToAll();
+					ServerQuestFile.INSTANCE.save();
+				}
 			}
-		}
-	}
-
-	@Override
-	public void onConfigSaved(ConfigGroup g, ICommandSender sender)
-	{
-		QuestObject object = ServerQuestFile.INSTANCE.get(id);
-
-		if (object != null)
-		{
-			ServerQuestFile.INSTANCE.clearCachedData();
-			ConfigGroup group = ConfigGroup.newGroup("object");
-			object.getConfig(group);
-			object.getExtraConfig(group);
-			new MessageEditObjectResponse(id, group.serializeNBT()).sendToAll();
-			ServerQuestFile.INSTANCE.save();
 		}
 	}
 }
