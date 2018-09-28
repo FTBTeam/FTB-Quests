01586443a36e39ce79cade0ba0027d879867c906
LatvianModder <latvianmodder@gmail.com>
2018-09-29 00:50:43 +0300

    Added button on top bar to claim all available rewards

--- src/main/java/com/feed_the_beast/ftbquests/gui/tree/PanelOtherButtons.java
+++ src/main/java/com/feed_the_beast/ftbquests/gui/tree/PanelOtherButtons.java
@@ -2,6 +2,9 @@ package com.feed_the_beast.ftbquests.gui.tree;
 
 import com.feed_the_beast.ftblib.lib.gui.Panel;
 import com.feed_the_beast.ftblib.lib.gui.WidgetLayout;
+import com.feed_the_beast.ftbquests.quest.Quest;
+import com.feed_the_beast.ftbquests.quest.QuestChapter;
+import com.feed_the_beast.ftbquests.quest.reward.QuestReward;
 
 /**
  * @author LatvianModder
@@ -16,9 +19,41 @@ public class PanelOtherButtons extends Panel
 		treeGui = (GuiQuestTree) panel.getGui();
 	}
 
+	private boolean hasRewards()
+	{
+		if (treeGui.questFile.self == null)
+		{
+			return false;
+		}
+
+		for (QuestChapter chapter : treeGui.questFile.chapters)
+		{
+			for (Quest quest : chapter.quests)
+			{
+				if (quest.isComplete(treeGui.questFile.self))
+				{
+					for (QuestReward reward : quest.rewards)
+					{
+						if (!treeGui.questFile.isRewardClaimed(reward))
+						{
+							return true;
+						}
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
 	@Override
 	public void addWidgets()
 	{
+		if (hasRewards())
+		{
+			add(new ButtonClaimAllRewards(this));
+		}
+
 		if (!treeGui.questFile.emergencyItems.isEmpty() && (treeGui.questFile.self != null || treeGui.questFile.canEdit()))
 		{
 			add(new ButtonEmergencyItems(this));
