3c344d7e322720a69cd479fe4339eda72bd8d3a2
LatvianModder <latvianmodder@gmail.com>
2018-12-06 16:21:12 +0200

    Added WIP csv export

--- src/main/java/com/feed_the_beast/ftbquests/gui/tree/ButtonEditSettings.java
+++ src/main/java/com/feed_the_beast/ftbquests/gui/tree/ButtonEditSettings.java
@@ -5,6 +5,7 @@ import com.feed_the_beast.ftblib.lib.gui.GuiBase;
 import com.feed_the_beast.ftblib.lib.gui.GuiHelper;
 import com.feed_the_beast.ftblib.lib.gui.GuiIcons;
 import com.feed_the_beast.ftblib.lib.gui.Panel;
+import com.feed_the_beast.ftblib.lib.util.FileUtils;
 import com.feed_the_beast.ftblib.lib.util.NBTUtils;
 import com.feed_the_beast.ftblib.lib.util.misc.MouseButton;
 import com.feed_the_beast.ftbquests.client.ClientQuestFile;
@@ -13,6 +14,8 @@ import com.feed_the_beast.ftbquests.gui.GuiRewardTables;
 import com.feed_the_beast.ftbquests.gui.GuiVariables;
 import com.feed_the_beast.ftbquests.net.MessageCompleteInstantly;
 import com.feed_the_beast.ftbquests.net.MessageResetProgress;
+import com.feed_the_beast.ftbquests.quest.Quest;
+import com.feed_the_beast.ftbquests.quest.QuestChapter;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.nbt.NBTTagCompound;
@@ -67,6 +70,70 @@ public class ButtonEditSettings extends ButtonTab
 				fileName.append(".nbt");
 				File file = new File(Minecraft.getMinecraft().gameDir, fileName.toString());
 				NBTUtils.writeNBT(file, nbt);
+				Minecraft.getMinecraft().player.sendMessage(new TextComponentTranslation("ftbquests.gui.saved_as_file", fileName.toString()));
+			}
+			catch (Exception ex)
+			{
+				ex.printStackTrace();
+			}
+		}));
+
+		contextMenu.add(new ContextMenuItem(I18n.format("ftbquests.gui.export_spreadsheet"), GuiIcons.DOWN, () -> {
+			try
+			{
+				int maxQuests = 0;
+
+				for (QuestChapter chapter : ClientQuestFile.INSTANCE.chapters)
+				{
+					maxQuests = Math.max(maxQuests, chapter.quests.size());
+				}
+
+				String[][] cells = new String[ClientQuestFile.INSTANCE.chapters.size() * 2][maxQuests + 1];
+
+				for (int x = 0; x < ClientQuestFile.INSTANCE.chapters.size(); x++)
+				{
+					QuestChapter c = ClientQuestFile.INSTANCE.chapters.get(x);
+					cells[x * 2][0] = c.getDisplayName().getUnformattedText();
+
+					for (int y = 0; y < c.quests.size(); y++)
+					{
+						Quest q = c.quests.get(y);
+						cells[x * 2][y + 1] = q.getDisplayName().getUnformattedText();
+					}
+				}
+
+				Calendar time = Calendar.getInstance();
+				StringBuilder fileName = new StringBuilder("local/ftbquests/saved/");
+				appendNum(fileName, time.get(Calendar.YEAR), '-');
+				appendNum(fileName, time.get(Calendar.MONTH) + 1, '-');
+				appendNum(fileName, time.get(Calendar.DAY_OF_MONTH), '-');
+				appendNum(fileName, time.get(Calendar.HOUR_OF_DAY), '-');
+				appendNum(fileName, time.get(Calendar.MINUTE), '-');
+				appendNum(fileName, time.get(Calendar.SECOND), '\0');
+				fileName.append(".csv");
+				File file = new File(Minecraft.getMinecraft().gameDir, fileName.toString());
+				List<String> csv = new ArrayList<>();
+
+				for (int y = 0; y < maxQuests + 1; y++)
+				{
+					StringBuilder builder = new StringBuilder();
+
+					for (int x = 0; x < ClientQuestFile.INSTANCE.chapters.size() * 2; x++)
+					{
+						if (x > 0)
+						{
+							builder.append(',');
+						}
+
+						builder.append('"');
+						builder.append(cells[x][y] == null ? "" : cells[x][y]);
+						builder.append('"');
+					}
+
+					csv.add(builder.toString());
+				}
+
+				FileUtils.saveSafe(file, csv);
 				Minecraft.getMinecraft().player.sendMessage(new TextComponentTranslation("ftbquests.gui.saved_as_file", fileName.toString()));
 			}
 			catch (Exception ex)
