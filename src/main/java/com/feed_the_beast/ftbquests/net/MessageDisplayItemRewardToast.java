7bff5b761bf0499c609ec75686eed40f9695fae7
LatvianModder <latvianmodder@gmail.com>
2018-12-09 00:57:12 +0200

    Added GUI that displays all rewards from Loot Crates and Loot Rewards

--- src/main/java/com/feed_the_beast/ftbquests/net/MessageDisplayItemRewardToast.java
+++ src/main/java/com/feed_the_beast/ftbquests/net/MessageDisplayItemRewardToast.java
@@ -1,10 +1,12 @@
 package com.feed_the_beast.ftbquests.net;
 
+import com.feed_the_beast.ftblib.lib.icon.Icon;
 import com.feed_the_beast.ftblib.lib.icon.ItemIcon;
 import com.feed_the_beast.ftblib.lib.io.DataIn;
 import com.feed_the_beast.ftblib.lib.io.DataOut;
 import com.feed_the_beast.ftblib.lib.net.MessageToClient;
 import com.feed_the_beast.ftblib.lib.net.NetworkWrapper;
+import com.feed_the_beast.ftbquests.gui.GuiRewardNotifications;
 import com.feed_the_beast.ftbquests.gui.tree.ToastReward;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
@@ -50,13 +52,21 @@ public class MessageDisplayItemRewardToast extends MessageToClient
 	@SideOnly(Side.CLIENT)
 	public void onMessage()
 	{
-		String s = stack.getDisplayName();
+		ItemStack stack1 = ItemHandlerHelper.copyStackWithSize(stack, 1);
+		Icon icon = ItemIcon.getItemIcon(stack1);
+		GuiRewardNotifications.RewardKey key = new GuiRewardNotifications.RewardKey(stack.getDisplayName(), icon);
+		key.stack = stack1;
 
-		if (stack.getCount() > 1)
+		if (!GuiRewardNotifications.add(key, stack.getCount()))
 		{
-			s = stack.getCount() + "x " + s;
-		}
+			String s = stack.getDisplayName();
 
-		Minecraft.getMinecraft().getToastGui().add(new ToastReward(stack.getRarity().color + s, ItemIcon.getItemIcon(ItemHandlerHelper.copyStackWithSize(stack, 1))));
+			if (stack.getCount() > 1)
+			{
+				s = stack.getCount() + "x " + s;
+			}
+
+			Minecraft.getMinecraft().getToastGui().add(new ToastReward(stack.getRarity().color + s, icon));
+		}
 	}
 }
