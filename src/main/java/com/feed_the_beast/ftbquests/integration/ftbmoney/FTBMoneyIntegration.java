7a47700940d1208fcd23706b29223e1b657dccf4
LatvianModder <latvianmodder@gmail.com>
2018-12-09 04:04:47 +0200

    Added FTB Money lock integration

--- src/main/java/com/feed_the_beast/ftbquests/integration/ftbmoney/FTBMoneyIntegration.java
+++ src/main/java/com/feed_the_beast/ftbquests/integration/ftbmoney/FTBMoneyIntegration.java
@@ -1,17 +1,23 @@
 package com.feed_the_beast.ftbquests.integration.ftbmoney;
 
 import com.feed_the_beast.ftblib.lib.config.ConfigString;
+import com.feed_the_beast.ftblib.lib.data.FTBLibAPI;
 import com.feed_the_beast.ftblib.lib.gui.IOpenableGui;
 import com.feed_the_beast.ftblib.lib.gui.misc.GuiEditConfigValue;
 import com.feed_the_beast.ftblib.lib.icon.Icon;
+import com.feed_the_beast.ftbquests.quest.ITeamData;
 import com.feed_the_beast.ftbquests.quest.Quest;
+import com.feed_the_beast.ftbquests.quest.QuestObject;
+import com.feed_the_beast.ftbquests.quest.ServerQuestFile;
 import com.feed_the_beast.ftbquests.quest.reward.FTBQuestsRewards;
 import com.feed_the_beast.ftbquests.quest.reward.QuestReward;
 import com.feed_the_beast.ftbquests.quest.reward.QuestRewardType;
 import com.feed_the_beast.ftbquests.quest.task.FTBQuestsTasks;
 import com.feed_the_beast.ftbquests.quest.task.QuestTaskType;
+import com.feed_the_beast.mods.money.EventShopLock;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Optional;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -74,5 +80,27 @@ public class FTBMoneyIntegration
 				}).openGui();
 			}
 		});
+	}
+
+	@SubscribeEvent
+	@Optional.Method(modid = "ftbmoney")
+	public static void shopLock(EventShopLock event)
+	{
+		if (event.getLock().startsWith("ftbquests:"))
+		{
+			QuestObject object = ServerQuestFile.INSTANCE.get(ServerQuestFile.INSTANCE.getID(event.getLock().substring(10)));
+
+			if (object == null)
+			{
+				return;
+			}
+
+			ITeamData data = ServerQuestFile.INSTANCE.getData(FTBLibAPI.getTeamID(event.getPlayer().getUniqueID()));
+
+			if (data != null && object.isComplete(data))
+			{
+				event.setCanceled(true);
+			}
+		}
 	}
 }
