ad210e57314690e4db2009ca4f99db8fa40f2258
LatvianModder <latvianmodder@gmail.com>
2019-03-11 05:41:35 +0200

    Added some items to PE NBT whitelist

--- src/main/java/com/feed_the_beast/ftbquests/integration/projecte/ProjectEIntegration.java
+++ src/main/java/com/feed_the_beast/ftbquests/integration/projecte/ProjectEIntegration.java
@@ -1,10 +1,14 @@
 package com.feed_the_beast.ftbquests.integration.projecte;
 
 import com.feed_the_beast.ftblib.lib.icon.Icon;
+import com.feed_the_beast.ftbquests.item.FTBQuestsItems;
 import com.feed_the_beast.ftbquests.quest.task.FTBQuestsTasks;
 import com.feed_the_beast.ftbquests.quest.task.QuestTaskType;
+import moze_intel.projecte.PECore;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
@@ -15,6 +19,9 @@ public class ProjectEIntegration
 	public static void preInit()
 	{
 		MinecraftForge.EVENT_BUS.register(ProjectEIntegration.class);
+		FMLInterModComms.sendMessage(PECore.MODID, "nbtwhitelist", new ItemStack(FTBQuestsItems.SCREEN));
+		FMLInterModComms.sendMessage(PECore.MODID, "nbtwhitelist", new ItemStack(FTBQuestsItems.PROGRESS_SCREEN));
+		FMLInterModComms.sendMessage(PECore.MODID, "nbtwhitelist", new ItemStack(FTBQuestsItems.LOOTCRATE));
 	}
 
 	@SubscribeEvent
