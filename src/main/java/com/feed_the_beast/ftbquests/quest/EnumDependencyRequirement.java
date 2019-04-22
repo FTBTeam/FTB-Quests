7acc5f41b3eaf365cc52f9930c14609d09cb7e1b
LatvianModder <latvianmodder@gmail.com>
2019-04-22 03:13:07 +0300

    Added quest visibility, closes #14

--- src/main/java/com/feed_the_beast/ftbquests/quest/EnumDependencyRequirement.java
+++ src/main/java/com/feed_the_beast/ftbquests/quest/EnumDependencyRequirement.java
@@ -1,21 +1,37 @@
 package com.feed_the_beast.ftbquests.quest;
 
+import com.feed_the_beast.ftblib.lib.util.IWithID;
+import com.feed_the_beast.ftblib.lib.util.misc.NameMap;
+import net.minecraft.util.text.TextComponentTranslation;
+
 /**
  * @author LatvianModder
  */
-public enum EnumDependencyRequirement
+public enum EnumDependencyRequirement implements IWithID
 {
-	ALL_COMPLETED(false, true),
-	ONE_COMPLETED(true, true),
-	ALL_STARTED(false, false),
-	ONE_STARTED(true, false);
+	ALL_COMPLETED("all_completed", false, true),
+	ONE_COMPLETED("one_completed", true, true),
+	ALL_STARTED("all_started", false, false),
+	ONE_STARTED("one_started", true, false);
 
+	public static final NameMap<EnumDependencyRequirement> NAME_MAP = NameMap.create(ALL_COMPLETED, NameMap.ObjectProperties.withName((sender, o) -> new TextComponentTranslation(o.langKey)), values());
+
+	private final String id;
+	private final String langKey;
 	public final boolean one;
 	public final boolean completed;
 
-	EnumDependencyRequirement(boolean o, boolean c)
+	EnumDependencyRequirement(String s, boolean o, boolean c)
 	{
+		id = s;
+		langKey = "ftbquests.quest.dependency_requirement." + id;
 		one = o;
 		completed = c;
+	}
+
+	@Override
+	public String getID()
+	{
+		return id;
 	}
 }
