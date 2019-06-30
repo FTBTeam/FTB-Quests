bf141b8085719aeb1c5e894b8ff4a569e02c75f5
LatvianModder <latvianmodder@gmail.com>
2019-06-30 15:46:52 +0300

    JEI Plugin hides invisible rewards and rewards with no ingredient

--- src/main/java/com/feed_the_beast/ftbquests/integration/jei/QuestRegistry.java
+++ src/main/java/com/feed_the_beast/ftbquests/integration/jei/QuestRegistry.java
@@ -3,8 +3,10 @@ package com.feed_the_beast.ftbquests.integration.jei;
 import com.feed_the_beast.ftbquests.client.ClientQuestFile;
 import com.feed_the_beast.ftbquests.quest.Chapter;
 import com.feed_the_beast.ftbquests.quest.Quest;
+import com.feed_the_beast.ftbquests.quest.reward.Reward;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author LatvianModder
@@ -34,9 +36,19 @@ public enum QuestRegistry
 			{
 				for (Quest quest : chapter.quests)
 				{
-					if (!quest.rewards.isEmpty())
+					List<Reward> rewards = new ArrayList<>();
+
+					for (Reward reward : quest.rewards)
 					{
-						QuestWrapper wrapper = new QuestWrapper(quest);
+						if (!reward.invisible && reward.getIngredient() != null)
+						{
+							rewards.add(reward);
+						}
+					}
+
+					if (!rewards.isEmpty())
+					{
+						QuestWrapper wrapper = new QuestWrapper(quest, rewards);
 						list.add(wrapper);
 						FTBQuestsJEIIntegration.runtime.getRecipeRegistry().addRecipe(wrapper, QuestCategory.UID);
 					}
