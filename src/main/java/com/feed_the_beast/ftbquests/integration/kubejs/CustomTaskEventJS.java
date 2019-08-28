ba029368064ec2ca8fb0512c9f2e448ef4eba2f4
LatvianModder <latvianmodder@gmail.com>
2019-08-28 03:49:51 +0300

    Added Copy button in NBT Editing GUI

--- src/main/java/com/feed_the_beast/ftbquests/integration/kubejs/CustomTaskEventJS.java
+++ src/main/java/com/feed_the_beast/ftbquests/integration/kubejs/CustomTaskEventJS.java
@@ -1,13 +1,19 @@
 package com.feed_the_beast.ftbquests.integration.kubejs;
 
+import dev.latvian.kubejs.documentation.DocClass;
+import dev.latvian.kubejs.documentation.DocField;
 import dev.latvian.kubejs.event.EventJS;
 
 /**
  * @author LatvianModder
  */
+@DocClass("Custom task check override event. You can use this to have custom condition combinations for quests")
 public class CustomTaskEventJS extends EventJS
 {
+	@DocField("Check callback - function (player), returns boolean")
 	public CustomTaskCheckerJS check;
+
+	@DocField("How often in ticks the callback function should be checked")
 	public int checkTimer = 1;
 
 	@Override
--- src/main/java/com/feed_the_beast/ftbquests/integration/kubejs/FTBQuestsKubeJSPlayerData.java
+++ src/main/java/com/feed_the_beast/ftbquests/integration/kubejs/FTBQuestsKubeJSPlayerData.java
@@ -3,12 +3,16 @@ package com.feed_the_beast.ftbquests.integration.kubejs;
 import com.feed_the_beast.ftblib.lib.util.NBTUtils;
 import com.feed_the_beast.ftbquests.FTBQuests;
 import com.feed_the_beast.ftbquests.net.MessageSyncEditingMode;
+import dev.latvian.kubejs.documentation.DocClass;
+import dev.latvian.kubejs.documentation.DocMethod;
+import dev.latvian.kubejs.documentation.Param;
 import dev.latvian.kubejs.player.PlayerDataJS;
 import net.minecraft.entity.player.EntityPlayerMP;
 
 /**
  * @author LatvianModder
  */
+@DocClass(displayName = "FTB Quests Player Data")
 public class FTBQuestsKubeJSPlayerData
 {
 	private final PlayerDataJS playerData;
@@ -18,12 +22,14 @@ public class FTBQuestsKubeJSPlayerData
 		playerData = p;
 	}
 
+	@DocMethod("Returns true if player is in editing mode")
 	public boolean canEdit()
 	{
 		EntityPlayerMP p = playerData.getPlayerEntity();
 		return p != null && FTBQuests.canEdit(p);
 	}
 
+	@DocMethod(value = "Sets editing mode for player", params = @Param("canEdit"))
 	public void setCanEdit(boolean canEdit)
 	{
 		EntityPlayerMP p = playerData.getPlayerEntity();
