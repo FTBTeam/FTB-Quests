3086a775b1b4aba87305aa7884cfad4b05657660
LatvianModder <latvianmodder@gmail.com>
2019-09-14 04:50:40 +0300

    KubeJS changes

--- src/main/java/com/feed_the_beast/ftbquests/integration/kubejs/CustomTaskEventJS.java
+++ src/main/java/com/feed_the_beast/ftbquests/integration/kubejs/CustomTaskEventJS.java
@@ -2,17 +2,18 @@ package com.feed_the_beast.ftbquests.integration.kubejs;
 
 import com.feed_the_beast.ftbquests.events.CustomTaskEvent;
 import com.feed_the_beast.ftbquests.quest.task.CustomTask;
-import dev.latvian.kubejs.documentation.DocClass;
-import dev.latvian.kubejs.documentation.DocMethod;
+import dev.latvian.kubejs.documentation.Ignore;
+import dev.latvian.kubejs.documentation.Info;
 import dev.latvian.kubejs.event.EventJS;
 
 /**
  * @author LatvianModder
  */
-@DocClass("Custom task check override event. You can use this to have custom condition combinations for quests")
+@Info("Custom task check override event. You can use this to have custom condition combinations for quests")
 public class CustomTaskEventJS extends EventJS
 {
-	public final transient CustomTaskEvent event;
+	@Ignore
+	public final CustomTaskEvent event;
 
 	CustomTaskEventJS(CustomTaskEvent e)
 	{
@@ -25,31 +26,30 @@ public class CustomTaskEventJS extends EventJS
 		return true;
 	}
 
-	@DocMethod
 	public CustomTask getTask()
 	{
 		return event.getTask();
 	}
 
-	@DocMethod("Check callback - function (player), is called every x ticks. You can change x with setCheckTimer()")
+	@Info("Check callback - function (player), is called every x ticks. You can change x with setCheckTimer()")
 	public void setCheck(CustomTaskCheckerJS c)
 	{
 		getTask().check = new CheckWrapper(c);
 	}
 
-	@DocMethod("How often in ticks the callback function should be checked")
+	@Info("How often in ticks the callback function should be checked")
 	public void setCheckTimer(int t)
 	{
 		getTask().checkTimer = t;
 	}
 
-	@DocMethod("Enable checking on button click")
+	@Info("Enable checking on button click")
 	public void setEnableButton(boolean b)
 	{
 		getTask().enableButton = b;
 	}
 
-	@DocMethod("Max progress of this task")
+	@Info("Max progress of this task")
 	public void setMaxProgress(long max)
 	{
 		getTask().maxProgress = max;
--- src/main/java/com/feed_the_beast/ftbquests/integration/kubejs/QuestObjectCompletedEventJS.java
+++ src/main/java/com/feed_the_beast/ftbquests/integration/kubejs/QuestObjectCompletedEventJS.java
@@ -3,8 +3,8 @@ package com.feed_the_beast.ftbquests.integration.kubejs;
 import com.feed_the_beast.ftbquests.events.ObjectCompletedEvent;
 import com.feed_the_beast.ftbquests.quest.QuestData;
 import com.feed_the_beast.ftbquests.quest.QuestObject;
-import dev.latvian.kubejs.documentation.DocClass;
-import dev.latvian.kubejs.documentation.DocMethod;
+import dev.latvian.kubejs.documentation.Ignore;
+import dev.latvian.kubejs.documentation.Info;
 import dev.latvian.kubejs.event.EventJS;
 import dev.latvian.kubejs.player.EntityArrayList;
 import dev.latvian.kubejs.server.ServerJS;
@@ -12,35 +12,34 @@ import dev.latvian.kubejs.server.ServerJS;
 /**
  * @author LatvianModder
  */
-@DocClass("Event that gets fired when an object is completed. It can be a file, quest, chapter, task")
+@Info("Event that gets fired when an object is completed. It can be a file, quest, chapter, task")
 public class QuestObjectCompletedEventJS extends EventJS
 {
-	public final transient ObjectCompletedEvent event;
+	@Ignore
+	public final ObjectCompletedEvent event;
 
 	public QuestObjectCompletedEventJS(ObjectCompletedEvent e)
 	{
 		event = e;
 	}
 
-	@DocMethod
 	public QuestData getData()
 	{
 		return event.getData();
 	}
 
-	@DocMethod
 	public QuestObject getObject()
 	{
 		return event.getObject();
 	}
 
-	@DocMethod("List of notified players. It isn't always the list of online members of that team, for example, this list is empty when invisible quest was completed")
+	@Info("List of notified players. It isn't always the list of online members of that team, for example, this list is empty when invisible quest was completed")
 	public EntityArrayList getNotifiedPlayers()
 	{
 		return ServerJS.instance.getOverworld().createEntityList(event.getNotifiedPlayers());
 	}
 
-	@DocMethod("List of all online team members")
+	@Info("List of all online team members")
 	public EntityArrayList getOnlineMembers()
 	{
 		return ServerJS.instance.getOverworld().createEntityList(getData().getOnlineMembers());
