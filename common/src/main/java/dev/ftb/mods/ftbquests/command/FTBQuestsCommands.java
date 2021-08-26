50ea6fc6c6332c7213f7461907e0b7366183903f
Max <maxh2709@gmail.com>
2021-08-26 22:16:39 +0200

    Add command to export / import loot tables to / from containers

--- common/src/main/java/dev/ftb/mods/ftbquests/command/FTBQuestsCommands.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/command/FTBQuestsCommands.java
@@ -3,6 +3,9 @@ package dev.ftb.mods.ftbquests.command;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import dev.ftb.mods.ftblibrary.config.Tristate;
 import dev.ftb.mods.ftbquests.FTBQuests;
 import dev.ftb.mods.ftbquests.net.CreateObjectResponsePacket;
@@ -13,6 +16,8 @@ import dev.ftb.mods.ftbquests.quest.QuestObjectBase;
 import dev.ftb.mods.ftbquests.quest.ServerQuestFile;
 import dev.ftb.mods.ftbquests.quest.TeamData;
 import dev.ftb.mods.ftbquests.quest.loot.RewardTable;
+import dev.ftb.mods.ftbquests.quest.loot.WeightedReward;
+import dev.ftb.mods.ftbquests.quest.reward.ItemReward;
 import dev.ftb.mods.ftbquests.quest.task.ItemTask;
 import dev.ftb.mods.ftbquests.util.FileUtils;
 import dev.ftb.mods.ftbquests.util.ProgressChange;
@@ -20,6 +25,8 @@ import me.shedaniel.architectury.registry.Registries;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
@@ -27,11 +34,14 @@ import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
@@ -44,6 +54,9 @@ import java.util.stream.Collectors;
  * @author LatvianModder
  */
 public class FTBQuestsCommands {
+
+	private static SimpleCommandExceptionType NO_INVENTORY = new SimpleCommandExceptionType(new TranslatableComponent("commands.ftbquests.command.error.no_inventory"));
+
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
 		dispatcher.register(Commands.literal("ftbquests")
 				.requires(s -> s.getServer().isSingleplayer() || s.hasPermission(2))
@@ -91,27 +104,118 @@ public class FTBQuestsCommands {
 								)
 						)
 				)
-				/*.then(Commands.literal("export_rewards_to_chest")
-						.then(Commands.argument("reward_table", StringArgumentType.word())
-								.executes(c -> exportRewards(c.getSource(), StringArgumentType.getString(c, "reward_table")))
+				.then(Commands.literal("export_reward_table_to_chest")
+						.requires(s -> s.hasPermission(2))
+						.then(Commands.argument("reward_table", QuestObjectArgument.questObject())
+								.executes(ctx -> {
+									QuestObjectBase table = ctx.getArgument("reward_table", QuestObjectBase.class);
+									if (!(table instanceof RewardTable)) {
+										throw QuestObjectArgument.NO_OBJECT.create(table.getCodeString());
+									}
+									return exportRewards(ctx.getSource(), (RewardTable) table, null);
+								})
+								.then(Commands.argument("pos", BlockPosArgument.blockPos())
+										.executes(ctx -> {
+											QuestObjectBase table = ctx.getArgument("reward_table", QuestObjectBase.class);
+											BlockPos pos = BlockPosArgument.getOrLoadBlockPos(ctx, "pos");
+											if (!(table instanceof RewardTable)) {
+												throw QuestObjectArgument.NO_OBJECT.create(table.getCodeString());
+											}
+											return exportRewards(ctx.getSource(), (RewardTable) table, pos);
+										})
+								)
 						)
 				)
-				.then(Commands.literal("import_rewards_from_chest")
-						.requires(permission)
-						.then(Commands.argument("reward_table", StringArgumentType.word())
-								.then(Commands.argument("weight", IntegerArgumentType.integer(1))
-										.then(Commands.argument("replace", BoolArgumentType.bool())
-												.executes(c -> importRewards(c.getSource(), StringArgumentType.getString(c, "reward_table"), IntegerArgumentType.getInteger(c, "weight"), BoolArgumentType.getBool(c, "replace")))
-										)
-										.executes(c -> importRewards(c.getSource(), StringArgumentType.getString(c, "reward_table"), IntegerArgumentType.getInteger(c, "weight"), false))
+				.then(Commands.literal("import_reward_table_from_chest")
+						.requires(s -> s.hasPermission(2))
+						.then(Commands.argument("name", StringArgumentType.string())
+								.executes(ctx -> {
+									String name = StringArgumentType.getString(ctx, "name");
+									return importRewards(ctx.getSource(), name, null);
+								})
+								.then(Commands.argument("pos", BlockPosArgument.blockPos())
+										.executes(ctx -> {
+											String name = StringArgumentType.getString(ctx, "name");
+											BlockPos pos = BlockPosArgument.getOrLoadBlockPos(ctx, "pos");
+											return importRewards(ctx.getSource(), name, pos);
+										})
 								)
-								.executes(c -> importRewards(c.getSource(), StringArgumentType.getString(c, "reward_table"), 1, false))
 						)
-				)*/
+				)
 				.then(Commands.literal("generate_chapter_with_all_items_in_game")
 						.executes(context -> generateAllItemChapter(context.getSource()))
 				)
 		);
+	}
+
+	private static int exportRewards(CommandSourceStack source, RewardTable table, BlockPos pos) throws CommandSyntaxException {
+		ServerPlayer player = source.getPlayerOrException();
+		ServerLevel level = source.getLevel();
+
+		if (pos == null) {
+			pos = new BlockPos(player.pick(10, 1F, false).getLocation());
+		}
+
+		BlockEntity be = level.getBlockEntity(pos);
+		if (!(be instanceof BaseContainerBlockEntity)) {
+			throw NO_INVENTORY.create();
+		}
+
+		BaseContainerBlockEntity container = (BaseContainerBlockEntity) be;
+		container.clearContent();
+
+		int s = 0;
+		for (WeightedReward reward : table.rewards) {
+			if (s >= container.getContainerSize()) {
+				source.sendSuccess(new TranslatableComponent("commands.ftbquests.command.feedback.table_too_many_items", table.getTitle()), false);
+				return 0;
+			} else if (!(reward.reward instanceof ItemReward)) {
+				continue;
+			}
+			container.setItem(s++, ((ItemReward) reward.reward).item);
+		}
+
+		source.sendSuccess(new TranslatableComponent("commands.ftbquests.command.feedback.table_imported", table.getTitle(), table.rewards.size()), false);
+
+		return 1;
+	}
+
+	private static int importRewards(CommandSourceStack source, String name, BlockPos pos) throws CommandSyntaxException {
+		ServerPlayer player = source.getPlayerOrException();
+		ServerLevel level = source.getLevel();
+		ServerQuestFile file = ServerQuestFile.INSTANCE;
+
+		if (pos == null) {
+			pos = new BlockPos(player.pick(10, 1F, false).getLocation());
+		}
+
+		RewardTable table = new RewardTable(file);
+		table.id = file.newID();
+		table.title = name;
+		table.icon = Items.CHEST.getDefaultInstance();
+
+		BlockEntity be = level.getBlockEntity(pos);
+		if (!(be instanceof BaseContainerBlockEntity)) {
+			throw NO_INVENTORY.create();
+		}
+
+		BaseContainerBlockEntity container = (BaseContainerBlockEntity) be;
+		for (int i = 0; i < container.getContainerSize(); i++) {
+			ItemStack stack = container.getItem(i);
+			if (!stack.isEmpty()) {
+				table.rewards.add(new WeightedReward(new ItemReward(table.fakeQuest, stack), 1));
+			}
+		}
+
+
+		file.rewardTables.add(table);
+		file.save();
+
+		new CreateObjectResponsePacket(table, null).sendToAll(level.getServer());
+
+		source.sendSuccess(new TranslatableComponent("commands.ftbquests.command.feedback.table_imported", name, table.rewards.size()), false);
+
+		return 1;
 	}
 
 	private static int editingMode(CommandSourceStack source, ServerPlayer player, @Nullable Boolean canEdit) {
