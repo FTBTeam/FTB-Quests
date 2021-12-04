344c6516f66c50fc2519d9f4aee40df777256138
Bob Varioa <true.email001@gmail.com>
2021-12-04 15:15:54 -0600

    feat: add config to exclude rewards from "Claim all"

--- common/src/main/java/dev/ftb/mods/ftbquests/quest/reward/Reward.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/quest/reward/Reward.java
@@ -34,11 +34,13 @@ public abstract class Reward extends QuestObjectBase {
 
 	public Tristate team;
 	public RewardAutoClaim autoclaim;
+	public boolean optional;
 
 	public Reward(Quest q) {
 		quest = q;
 		team = Tristate.DEFAULT;
 		autoclaim = RewardAutoClaim.DEFAULT;
+		optional = getType().getExcludeFromListRewards();
 	}
 
 	@Override
@@ -75,6 +77,8 @@ public abstract class Reward extends QuestObjectBase {
 		if (autoclaim != RewardAutoClaim.DEFAULT) {
 			nbt.putString("auto", autoclaim.id);
 		}
+
+		if (optional) nbt.putBoolean("optional", optional);
 	}
 
 	@Override
@@ -82,6 +86,7 @@ public abstract class Reward extends QuestObjectBase {
 		super.readData(nbt);
 		team = Tristate.read(nbt, "team_reward");
 		autoclaim = RewardAutoClaim.NAME_MAP.get(nbt.getString("auto"));
+		optional = nbt.getBoolean("optional");
 	}
 
 	@Override
@@ -89,6 +94,7 @@ public abstract class Reward extends QuestObjectBase {
 		super.writeNetData(buffer);
 		Tristate.NAME_MAP.write(buffer, team);
 		RewardAutoClaim.NAME_MAP.write(buffer, autoclaim);
+		buffer.writeBoolean(optional);
 	}
 
 	@Override
@@ -96,6 +102,7 @@ public abstract class Reward extends QuestObjectBase {
 		super.readNetData(buffer);
 		team = Tristate.NAME_MAP.read(buffer);
 		autoclaim = RewardAutoClaim.NAME_MAP.read(buffer);
+		optional = buffer.readBoolean();
 	}
 
 	@Override
@@ -104,6 +111,7 @@ public abstract class Reward extends QuestObjectBase {
 		super.getConfig(config);
 		config.addEnum("team", team, v -> team = v, Tristate.NAME_MAP).setNameKey("ftbquests.reward.team_reward");
 		config.addEnum("autoclaim", autoclaim, v -> autoclaim = v, RewardAutoClaim.NAME_MAP).setNameKey("ftbquests.reward.autoclaim");
+		config.addBool("optional", optional, v -> optional = v, optional).setNameKey("ftbquests.reward.optional");
 	}
 
 	public abstract void claim(ServerPlayer player, boolean notify);
@@ -233,7 +241,7 @@ public abstract class Reward extends QuestObjectBase {
 	}
 
 	public boolean getExcludeFromClaimAll() {
-		return getType().getExcludeFromListRewards();
+		return optional;
 	}
 
 	@Nullable
