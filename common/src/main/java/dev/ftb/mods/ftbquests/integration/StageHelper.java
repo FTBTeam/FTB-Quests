670c30c0c834be246e93a2f13b94e408aee1011b
Max <maxh2709@gmail.com>
2022-07-17 14:09:25 +0200

    Reimplement Game Stages integration

--- common/src/main/java/dev/ftb/mods/ftbquests/integration/StageHelper.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/integration/StageHelper.java
@@ -1,18 +1,14 @@
 package dev.ftb.mods.ftbquests.integration;
 
+import com.google.common.base.Suppliers;
 import dev.architectury.platform.Platform;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.util.LazyLoadedValue;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.function.Supplier;
+
 public abstract class StageHelper {
-	public static LazyLoadedValue<StageHelper> instance = new LazyLoadedValue<>(() -> {
-		if (Platform.isModLoaded("kubejs")) {
-			return new KubeJSStageHelper();
-		} else {
-			return new EntityTagStageHelper();
-		}
-	});
+	public static Supplier<? extends StageHelper> instance = Suppliers.memoize(() -> Platform.isModLoaded("kubejs") ? new KubeJSStageHelper() : new EntityTagStageHelper());
 
 	public abstract boolean has(Player player, String stage);
 
