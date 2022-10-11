47f753a018dd537f4051b08324f097f8741f2cf2
Des Herriott <des.herriott@gmail.com>
2022-10-11 10:38:46 +0100

    feat: support structure tags in StructureTask
    To use, prefix the structure with "#" as normal
    - also fixes crashing on bad resource locations
    
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/491

--- common/src/main/java/dev/ftb/mods/ftbquests/quest/task/StructureTask.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/quest/task/StructureTask.java
@@ -1,13 +1,15 @@
 package dev.ftb.mods.ftbquests.quest.task;
 
+import com.mojang.datafixers.util.Either;
 import dev.ftb.mods.ftblibrary.config.ConfigGroup;
+import dev.ftb.mods.ftbquests.FTBQuests;
 import dev.ftb.mods.ftbquests.quest.Quest;
 import dev.ftb.mods.ftbquests.quest.TeamData;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.ChatFormatting;
+import net.minecraft.ResourceLocationException;
 import net.minecraft.core.Registry;
-import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.MutableComponent;
@@ -17,18 +19,20 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
-import net.minecraft.world.level.levelgen.structure.StructureStart;
 
 /**
  * @author MaxNeedsSnacks
  */
 public class StructureTask extends BooleanTask {
-	public ResourceKey<ConfiguredStructureFeature<?, ?>> structure;
+	private static final ResourceLocation DEFAULT_STRUCTURE = new ResourceLocation("minecraft:mineshaft");
+
+	private Either<ResourceKey<ConfiguredStructureFeature<?, ?>>, TagKey<ConfiguredStructureFeature<?, ?>>> structure;
 
 	public StructureTask(Quest quest) {
 		super(quest);
-		structure = ResourceKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, new ResourceLocation("minecraft:mineshaft"));
+		structure = Either.left(ResourceKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, DEFAULT_STRUCTURE));
 	}
 
 	@Override
@@ -39,38 +43,39 @@ public class StructureTask extends BooleanTask {
 	@Override
 	public void writeData(CompoundTag nbt) {
 		super.writeData(nbt);
-		nbt.putString("structure", structure.location().toString());
+		nbt.putString("structure", getStructure());
 	}
 
 	@Override
 	public void readData(CompoundTag nbt) {
 		super.readData(nbt);
-		structure = ResourceKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, new ResourceLocation(nbt.getString("structure")));
+		setStructure(nbt.getString("structure"));
 	}
 
 	@Override
 	public void writeNetData(FriendlyByteBuf buffer) {
 		super.writeNetData(buffer);
-		buffer.writeResourceLocation(structure.location());
+		buffer.writeUtf(getStructure());
 	}
 
 	@Override
 	public void readNetData(FriendlyByteBuf buffer) {
 		super.readNetData(buffer);
-		structure = ResourceKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, buffer.readResourceLocation());
+		setStructure(buffer.readUtf(1024));
 	}
 
 	@Override
 	@Environment(EnvType.CLIENT)
 	public void getConfig(ConfigGroup config) {
 		super.getConfig(config);
-		config.addString("structure", structure.location().toString(), v -> structure = ResourceKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, new ResourceLocation(v)), "minecraft:mineshaft");
+		config.addString("structure", getStructure(), this::setStructure, "minecraft:mineshaft");
 	}
 
 	@Override
 	@Environment(EnvType.CLIENT)
 	public MutableComponent getAltTitle() {
-		return new TranslatableComponent("ftbquests.task.ftbquests.structure").append(": ").append(new TextComponent(structure.location().toString()).withStyle(ChatFormatting.DARK_GREEN));
+		return new TranslatableComponent("ftbquests.task.ftbquests.structure")
+				.append(": ").append(new TextComponent(getStructure()).withStyle(ChatFormatting.DARK_GREEN));
 	}
 
 	@Override
@@ -80,6 +85,43 @@ public class StructureTask extends BooleanTask {
 
 	@Override
 	public boolean canSubmit(TeamData teamData, ServerPlayer player) {
-		return ((ServerLevel) player.level).structureFeatureManager().getStructureWithPieceAt(player.blockPosition(), structure).isValid();
+		ServerLevel level = (ServerLevel) player.level;
+		return structure.map(
+				key -> level.structureFeatureManager().getStructureWithPieceAt(player.blockPosition(), key).isValid(),
+				tag -> {
+					var reg = level.registryAccess().registry(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY).orElseThrow();
+					for (var holder : reg.getTagOrEmpty(tag)) {
+						if (level.structureFeatureManager().getStructureWithPieceAt(player.blockPosition(), holder.value()).isValid()) {
+							return true;
+						}
+					}
+					return false;
+				}
+		);
 	}
+
+	private void setStructure(String resLoc) {
+		structure = resLoc.startsWith("#") ?
+				Either.right(TagKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, safeResourceLocation(resLoc.substring(1)))) :
+				Either.left(ResourceKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, safeResourceLocation(resLoc)));
+	}
+
+	private String getStructure() {
+		return structure.map(structure -> structure.location().toString(), tag -> "#" + tag.location().toString());
+	}
+
+	private ResourceLocation safeResourceLocation(String str) {
+		try {
+			return new ResourceLocation(str);
+		} catch (ResourceLocationException e) {
+			if (getQuestFile().isServerSide()) {
+				FTBQuests.LOGGER.warn("Ignoring bad structure resource location '{}' for structure task {}", str, id);
+			} else {
+				FTBQuests.PROXY.getClientPlayer().displayClientMessage(
+						new TextComponent("Ignoring bad structure resource location: " + str).withStyle(ChatFormatting.RED), false);
+			}
+			return DEFAULT_STRUCTURE;
+		}
+	}
+
 }
