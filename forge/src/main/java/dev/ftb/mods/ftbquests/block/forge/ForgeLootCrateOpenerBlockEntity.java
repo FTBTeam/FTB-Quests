25d7a5fcc6de5d6cd7c511ffff58fb95b30dc43e
Des Herriott <des.herriott@gmail.com>
2022-11-04 16:04:21 +0000

    chore: stop using deprecated forge capability

--- forge/src/main/java/dev/ftb/mods/ftbquests/block/forge/ForgeLootCrateOpenerBlockEntity.java
+++ forge/src/main/java/dev/ftb/mods/ftbquests/block/forge/ForgeLootCrateOpenerBlockEntity.java
@@ -6,8 +6,8 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -23,7 +23,7 @@ public class ForgeLootCrateOpenerBlockEntity extends LootCrateOpenerBlockEntity 
     @NotNull
     @Override
     public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
-        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+        if (cap == ForgeCapabilities.ITEM_HANDLER) {
             return itemCap.cast();
         }
         return super.getCapability(cap, side);
