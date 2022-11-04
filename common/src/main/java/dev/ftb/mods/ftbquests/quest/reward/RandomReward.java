46d98d0b99db803ea967372a10cf317afe7e96fb
Des Herriott <des.herriott@gmail.com>
2022-11-04 08:44:32 +0000

    chore: minor code cleanup & method documentation [ci skip]

--- common/src/main/java/dev/ftb/mods/ftbquests/quest/reward/RandomReward.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/quest/reward/RandomReward.java
@@ -197,11 +197,11 @@ public class RandomReward extends Reward {
 	}
 
 	@Override
-	public boolean automatedClaimPre(BlockEntity tileEntity, List<ItemStack> items, Random random, UUID playerId, @Nullable ServerPlayer player) {
+	public boolean automatedClaimPre(BlockEntity blockEntity, List<ItemStack> items, Random random, UUID playerId, @Nullable ServerPlayer player) {
 		return false;
 	}
 
 	@Override
-	public void automatedClaimPost(BlockEntity tileEntity, UUID playerId, @Nullable ServerPlayer player) {
+	public void automatedClaimPost(BlockEntity blockEntity, UUID playerId, @Nullable ServerPlayer player) {
 	}
 }
