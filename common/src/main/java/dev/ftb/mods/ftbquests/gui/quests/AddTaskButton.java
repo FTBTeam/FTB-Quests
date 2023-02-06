ba5a3c8fcf3db57a4c35370e9f2dc4761c03ab1f
Des Herriott <des.herriott@gmail.com>
2023-02-06 10:43:51 +0000

    feat: tasks can now also be copied
    Similar to quest: copy the task ID, and a "Paste Task" option
    will be available when creating tasks via the view quest panel
    add button, and also via the quest screen context menu
    
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/176

--- common/src/main/java/dev/ftb/mods/ftbquests/gui/quests/AddTaskButton.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/gui/quests/AddTaskButton.java
@@ -1,18 +1,24 @@
 package dev.ftb.mods.ftbquests.gui.quests;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import dev.ftb.mods.ftblibrary.icon.Icons;
 import dev.ftb.mods.ftblibrary.ui.Button;
 import dev.ftb.mods.ftblibrary.ui.ContextMenuItem;
 import dev.ftb.mods.ftblibrary.ui.Panel;
 import dev.ftb.mods.ftblibrary.ui.Theme;
 import dev.ftb.mods.ftblibrary.ui.input.MouseButton;
+import dev.ftb.mods.ftbquests.FTBQuests;
 import dev.ftb.mods.ftbquests.net.CreateObjectMessage;
 import dev.ftb.mods.ftbquests.quest.Quest;
+import dev.ftb.mods.ftbquests.quest.QuestObjectBase;
+import dev.ftb.mods.ftbquests.quest.task.Task;
 import dev.ftb.mods.ftbquests.quest.task.TaskType;
 import dev.ftb.mods.ftbquests.quest.task.TaskTypes;
 import dev.ftb.mods.ftbquests.quest.theme.property.ThemeProperties;
+import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,13 +51,46 @@ public class AddTaskButton extends Button {
 			}));
 		}
 
+		String clip = getClipboardString();
+		if (!clip.isEmpty()) {
+			try {
+				long taskId = Long.valueOf(clip, 16);
+				if (FTBQuests.PROXY.getQuestFile(true).get(taskId) instanceof Task task) {
+					contextMenu.add(ContextMenuItem.SEPARATOR);
+					contextMenu.add(new PasteTaskMenuItem(task, () -> copyAndCreateTask(task)));
+				}
+			} catch (NumberFormatException ignored) {
+			}
+		}
+
 		getGui().openContextMenu(contextMenu);
+	}
+
+	private void copyAndCreateTask(Task task) {
+		Task task2 = TaskType.createTask(quest, task.getType().id.toString());
+		if (task2 != null) {
+			CompoundTag tag = new CompoundTag();
+			task.writeData(tag);
+			task2.readData(tag);
+			CompoundTag extra = new CompoundTag();
+			extra.putString("type", task2.getType().getTypeForNBT());
+			new CreateObjectMessage(task2, extra).sendToServer();
+		}
 	}
 
 	@Override
 	public void drawBackground(PoseStack matrixStack, Theme theme, int x, int y, int w, int h) {
 		if (isMouseOver()) {
 			super.drawBackground(matrixStack, theme, x, y, w, h);
+		}
+	}
+
+	public static class PasteTaskMenuItem extends TooltipContextMenuItem {
+		public PasteTaskMenuItem(Task task, @Nullable Runnable callback) {
+			super(Component.translatable("ftbquests.gui.paste_task"), Icons.ADD, callback,
+					Component.literal("\"").append(task.getTitle()).append("\""),
+					Component.literal(QuestObjectBase.getCodeString(task.id)).withStyle(ChatFormatting.DARK_GRAY)
+			);
 		}
 	}
 }
