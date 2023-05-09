1b3527ee4656024e04d6a237075f37c82bda20ce
Des Herriott <des.herriott@gmail.com>
2023-05-09 11:33:00 +0100

    fix: another possible fix for items not being detected
    Use real time from System.nanoTime() rather than overworld ticks, which
    could be broken by /gamerule doDaylightCycle being false

--- common/src/main/java/dev/ftb/mods/ftbquests/util/DeferredInventoryDetection.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/util/DeferredInventoryDetection.java
@@ -6,43 +6,36 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 public class DeferredInventoryDetection {
+    private static final long MILLIS_IN_TICK = 50L;
     private static final Object2LongOpenHashMap<UUID> playerMap = new Object2LongOpenHashMap<>();
-    private static Level overworld = null;
 
     public static void tick(MinecraftServer server) {
-        Set<UUID> toRemove = new HashSet<>();
+        if (!playerMap.isEmpty()) {
+            Set<UUID> toRemove = new HashSet<>();
+            long now = System.currentTimeMillis();
 
-        for (UUID id : playerMap.keySet()) {
-            if (playerMap.getLong(id) <= overworld(server).getGameTime()) {
-                ServerPlayer sp = server.getPlayerList().getPlayer(id);
-                if (sp != null) {
-                    FTBQuestsInventoryListener.detect(sp, ItemStack.EMPTY, 0);
+            for (UUID id : playerMap.keySet()) {
+                if (now >= playerMap.getLong(id)) {
+                    ServerPlayer sp = server.getPlayerList().getPlayer(id);
+                    if (sp != null) {
+                        FTBQuestsInventoryListener.detect(sp, ItemStack.EMPTY, 0);
+                    }
+                    toRemove.add(id);
                 }
-                toRemove.add(id);
             }
-        }
 
-        toRemove.forEach(playerMap::removeLong);
+            toRemove.forEach(playerMap::removeLong);
+        }
     }
 
     static void scheduleInventoryCheck(ServerPlayer sp) {
-        if (sp.getServer() != null) {
-            int delay = Mth.clamp(ServerQuestFile.INSTANCE.detectionDelay, 1, 200);
-            playerMap.putIfAbsent(sp.getUUID(), overworld(sp.getServer()).getGameTime() + delay);
-        }
-    }
-
-    private static Level overworld(MinecraftServer server) {
-        if (overworld == null) {
-            overworld = server.getLevel(Level.OVERWORLD);
-        }
-        return overworld;
+        int delay = Mth.clamp(ServerQuestFile.INSTANCE.detectionDelay, 1, 200);
+        playerMap.putIfAbsent(sp.getUUID(), System.currentTimeMillis() + delay * MILLIS_IN_TICK);
     }
 }
