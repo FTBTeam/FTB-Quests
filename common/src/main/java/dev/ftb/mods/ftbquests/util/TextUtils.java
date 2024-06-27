08671c8b74ec7c85292bdc5f3e78593d4be75f7c
Des Herriott <des.herriott@gmail.com>
2024-06-27 09:26:05 +0100

    fix: some raw json text in quest descriptions was not being recognised
    Backported fix from 1.21
    
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/1250

--- common/src/main/java/dev/ftb/mods/ftbquests/util/TextUtils.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/util/TextUtils.java
@@ -2,32 +2,29 @@ package dev.ftb.mods.ftbquests.util;
 
 import com.google.gson.JsonParseException;
 import dev.ftb.mods.ftblibrary.util.client.ClientTextComponentUtils;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-
-import java.util.regex.Pattern;
+import net.minecraft.network.chat.MutableComponent;
 
 public class TextUtils {
-    private static final Pattern JSON_TEXT_PATTERN = Pattern.compile("^[{\\[]\\s*\"");
-
     /**
-     * Parse some rich text into a Component. Use vanilla-style raw JSON if possible, fall back to old-style FTB
+     * Parse some rich text into a Component. Use vanilla-style raw JSON if applicable, fall back to old-style FTB
      * Quests rich text otherwise. (FTB Quests rich text is more concise, raw JSON is much more powerful)
      *
      * @param str the raw string to parse
      * @return a component, which could be the error message if parsing failed
      */
     public static Component parseRawText(String str) {
-        return JSON_TEXT_PATTERN.matcher(str).find() ?
-                deserializeRawJsonText(str) :
-                ClientTextComponentUtils.parse(str);
-    }
-
-    private static Component deserializeRawJsonText(String raw) {
-        try {
-            return Component.Serializer.fromJson(raw);
-        } catch (JsonParseException e) {
-            return Component.literal("ERROR: " + e.getMessage()).withStyle(ChatFormatting.RED);
+        String str2 = str.trim();
+        if (str2.startsWith("[") && str2.endsWith("]") || str2.startsWith("{") && str2.endsWith("}")) {
+            // could be JSON raw text, but not for definite...
+            try {
+                MutableComponent res = Component.Serializer.fromJson(str2);
+                if (res != null) {
+                    return res;
+                }
+            } catch (JsonParseException ignored) {
+            }
         }
+        return ClientTextComponentUtils.parse(str);
     }
 }
