8ccaeed59d983de9d46ed191848df056b9c524ac
Des Herriott <des.herriott@gmail.com>
2024-06-27 09:26:05 +0100

    fix: some raw json text in quest descriptions was not being recognised
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/1239

--- common/src/main/java/dev/ftb/mods/ftbquests/util/TextUtils.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/util/TextUtils.java
@@ -1,40 +1,40 @@
 package dev.ftb.mods.ftbquests.util;
 
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
 import dev.ftb.mods.ftblibrary.util.client.ClientTextComponentUtils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
-import net.minecraft.nbt.TagType;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class TextUtils {
-    private static final Pattern JSON_TEXT_PATTERN = Pattern.compile("^[{\\[]\\s*\"");
-
     /**
-     * Parse some rich text into a Component. Use vanilla-style raw JSON if possible, fall back to old-style FTB
+     * Parse some rich text into a Component. Use vanilla-style raw JSON if applicable, fall back to old-style FTB
      * Quests rich text otherwise. (FTB Quests rich text is more concise, raw JSON is much more powerful)
      *
      * @param str the raw string to parse
      * @return a component, which could be the error message if parsing failed
      */
     public static Component parseRawText(String str, HolderLookup.Provider provider) {
-        return JSON_TEXT_PATTERN.matcher(str).find() ?
-                deserializeRawJsonText(str, provider) :
-                ClientTextComponentUtils.parse(str);
-    }
+        String str2 = str.trim();
+        if (str2.startsWith("[") && str2.endsWith("]") || str2.startsWith("{") && str2.endsWith("}")) {
+            // could be JSON raw text, but not for definite...
+            try {
+                MutableComponent res = Component.Serializer.fromJson(str2, provider);
+                if (res != null) {
+                    return res;
+                }
+            } catch (JsonParseException ignored) {
 
-    private static Component deserializeRawJsonText(String raw, HolderLookup.Provider provider) {
-        try {
-            return Component.Serializer.fromJson(raw, provider);
-        } catch (JsonParseException e) {
-            return Component.literal("ERROR: " + e.getMessage()).withStyle(ChatFormatting.RED);
+            }
         }
+        return ClientTextComponentUtils.parse(str);
     }
 
     public static List<String> fromListTag(ListTag tag) {
