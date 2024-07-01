d6ca06a672e8f41924d6dbda3617116a720291a2
Des Herriott <des.herriott@gmail.com>
2024-07-01 14:38:02 +0100

    fix: parent chapter/group creation popup panels to the quest screen
    Otherwise they are drawn relative to the chapter panel, which can pop
    in and out...

--- common/src/main/java/dev/ftb/mods/ftbquests/client/gui/quests/ChapterPanel.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/client/gui/quests/ChapterPanel.java
@@ -189,7 +189,7 @@ public class ChapterPanel extends Panel {
 				List<ContextMenuItem> contextMenu = new ArrayList<>();
 				contextMenu.add(new ContextMenuItem(Component.translatable("ftbquests.chapter"), ThemeProperties.ADD_ICON.get(), b -> {
 					StringConfig c = new StringConfig(Pattern.compile("^.+$"));
-					EditStringConfigOverlay<String> overlay = new EditStringConfigOverlay<>(parent, c, accepted -> {
+					EditStringConfigOverlay<String> overlay = new EditStringConfigOverlay<>(parent.getParent(), c, accepted -> {
 						chapterPanel.questScreen.openGui();
 
 						if (accepted && !c.getValue().isEmpty()) {
@@ -209,7 +209,7 @@ public class ChapterPanel extends Panel {
 
 				contextMenu.add(new ContextMenuItem(Component.translatable("ftbquests.chapter_group"), ThemeProperties.ADD_ICON.get(), b -> {
 					StringConfig c = new StringConfig(Pattern.compile("^.+$"));
-					EditStringConfigOverlay<String> overlay = new EditStringConfigOverlay<>(parent, c, accepted -> {
+					EditStringConfigOverlay<String> overlay = new EditStringConfigOverlay<>(parent.getParent(), c, accepted -> {
 						chapterPanel.questScreen.openGui();
 
 						if (accepted) {
@@ -286,7 +286,7 @@ public class ChapterPanel extends Panel {
 				playClickSound();
 
 				StringConfig c = new StringConfig(Pattern.compile("^.+$"));
-				EditStringConfigOverlay<String> overlay = new EditStringConfigOverlay<>(parent, c, accepted -> {
+				EditStringConfigOverlay<String> overlay = new EditStringConfigOverlay<>(parent.getParent(), c, accepted -> {
 					chapterPanel.questScreen.openGui();
 
 					if (accepted && !c.getValue().isEmpty()) {
