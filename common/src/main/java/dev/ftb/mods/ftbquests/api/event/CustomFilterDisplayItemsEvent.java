42a2c08354a04d25b65a018198b0e1297345e6d2
Des Herriott <des.herriott@gmail.com>
2024-07-01 15:59:22 +0100

    chore: added some javadoc to CustomFilterDisplayItemsEvent

--- common/src/main/java/dev/ftb/mods/ftbquests/api/event/CustomFilterDisplayItemsEvent.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/api/event/CustomFilterDisplayItemsEvent.java
@@ -2,11 +2,27 @@ package dev.ftb.mods.ftbquests.api.event;
 
 import dev.architectury.event.Event;
 import dev.architectury.event.EventFactory;
+import dev.ftb.mods.ftbquests.FTBQuests;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.Collection;
 import java.util.function.Consumer;
 
+/**
+ * This event is fired on the client to allow mods to register custom items to be displayed when filters (such as the
+ * Smart Filter from FTB Filter System) look for matching items for their filter. By default, only items in the creative
+ * search tab are checked for, so this event is useful for adding items with custom data components.
+ * <br>
+ * KubeJS integration for this event is provided via FTB XMod Compat, and is the typical way for modpack makers to
+ * add custom display items for any filters they create. An example KubeJS snippet (to be placed in
+ * kubejs/client_scripts) to register an item:
+ * <pre>
+ *   FTBQuestsEvents.customFilterItem(event => {
+ *      event.addStack('minecraft:iron_axe {display:{Name:{text:\"Test Axe!\"}}, Damage: 50}')
+ *      event.addStack('minecraft:diamond_axe {display:{Name:{text:\"Test Axe 2!\"}}, Damage: 300}')
+ *   })
+ * </pre>
+ */
 public class CustomFilterDisplayItemsEvent {
     private final Consumer<ItemStack> callback;
 
@@ -21,6 +37,6 @@ public class CustomFilterDisplayItemsEvent {
     }
 
     public void add(Collection<ItemStack> stacks) {
-        stacks.forEach(callback::accept);
+        stacks.forEach(callback);
     }
 }
