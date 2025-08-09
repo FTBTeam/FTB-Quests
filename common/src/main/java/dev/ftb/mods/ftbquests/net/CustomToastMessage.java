0c2f91af31fec283e1881f535aa3812c74ec5ae7
Des Herriott <des.herriott@gmail.com>
2025-08-09 17:46:06 +0100

    fix: toast reward using client-only methods
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/1744

--- common/src/main/java/dev/ftb/mods/ftbquests/net/CustomToastMessage.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/net/CustomToastMessage.java
@@ -1,22 +1,20 @@
 package dev.ftb.mods.ftbquests.net;
 
 import dev.architectury.networking.NetworkManager;
-import dev.ftb.mods.ftblibrary.icon.Icon;
 import dev.ftb.mods.ftbquests.api.FTBQuestsAPI;
+import dev.ftb.mods.ftbquests.client.ClientQuestFile;
 import dev.ftb.mods.ftbquests.client.FTBQuestsNetClient;
+import dev.ftb.mods.ftbquests.quest.reward.ToastReward;
 import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.ComponentSerialization;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 
-public record CustomToastMessage(Component title, Component text, Icon icon) implements CustomPacketPayload {
+public record CustomToastMessage(long id) implements CustomPacketPayload {
 	public static final Type<CustomToastMessage> TYPE = new Type<>(FTBQuestsAPI.rl("custom_toast_message"));
 
 	public static final StreamCodec<RegistryFriendlyByteBuf, CustomToastMessage> STREAM_CODEC = StreamCodec.composite(
-			ComponentSerialization.STREAM_CODEC, CustomToastMessage::title,
-			ComponentSerialization.STREAM_CODEC, CustomToastMessage::text,
-			Icon.STREAM_CODEC, CustomToastMessage::icon,
+			ByteBufCodecs.VAR_LONG, CustomToastMessage::id,
 			CustomToastMessage::new
 	);
 
@@ -26,6 +24,10 @@ public record CustomToastMessage(Component title, Component text, Icon icon) imp
 	}
 
 	public static void handle(CustomToastMessage message, NetworkManager.PacketContext context) {
-		context.queue(() -> FTBQuestsNetClient.displayCustomToast(message.title, message.text, message.icon));
+		context.queue(() -> {
+			if (ClientQuestFile.exists() && ClientQuestFile.INSTANCE.getBase(message.id) instanceof ToastReward toastReward) {
+				FTBQuestsNetClient.displayCustomToast(toastReward);
+			}
+		});
 	}
 }
