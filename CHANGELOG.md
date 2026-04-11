7edca470745906c0b7cb26e57cdb0c882453d20e
Michael Hillcox <michaelhillcox@gmail.com>
2026-04-11 11:53:56 +0100

    fix: toasts not working ftbteam/ftb-mods-issues#2025

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2001.4.21]
+
+### Fixed
+* [#2025](https://github.com/FTBTeam/FTB-Mods-Issues/issues/2025) Reward toasts not working.
+
 ## [2001.4.20]
 
 ### Fixed
--- common/src/main/java/dev/ftb/mods/ftbquests/client/gui/RewardToast.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/client/gui/RewardToast.java
@@ -8,17 +8,23 @@ import net.minecraft.network.chat.Component;
  * @author LatvianModder
  */
 public class RewardToast extends SimpleToast {
+	private final Component title;
 	private final Component text;
 	private final Icon icon;
 
-	public RewardToast(Component t, Icon i) {
-		text = t;
-		icon = i;
+	public RewardToast(Component text, Icon icon) {
+		this(Component.translatable("ftbquests.reward.collected"), text, icon);
+	}
+
+	public RewardToast(Component title, Component text, Icon icon) {
+		this.title = title;
+		this.text = text;
+		this.icon = icon;
 	}
 
 	@Override
 	public Component getTitle() {
-		return Component.translatable("ftbquests.reward.collected");
+		return title;
 	}
 
 	@Override
