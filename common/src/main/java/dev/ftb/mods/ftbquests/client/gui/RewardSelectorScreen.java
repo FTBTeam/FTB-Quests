14d044c85904299c4880da95b4829bbe2b87d1e9
Des Herriott <des.herriott@gmail.com>
2026-05-11 09:05:44 +0100

    feat: added "Claim XP" button to reward selector screen

--- common/src/main/java/dev/ftb/mods/ftbquests/client/gui/RewardSelectorScreen.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/client/gui/RewardSelectorScreen.java
@@ -7,6 +7,7 @@ import dev.ftb.mods.ftblibrary.client.icon.IconHelper;
 import dev.ftb.mods.ftblibrary.client.util.ClientUtils;
 import dev.ftb.mods.ftblibrary.icon.Color4I;
 import dev.ftb.mods.ftblibrary.icon.Icons;
+import dev.ftb.mods.ftblibrary.icon.ItemIcon;
 import dev.ftb.mods.ftblibrary.platform.network.Play2ServerNetworking;
 import dev.ftb.mods.ftblibrary.util.TooltipList;
 import dev.ftb.mods.ftbquests.client.ClientQuestFile;
@@ -17,6 +18,7 @@ import dev.ftb.mods.ftbquests.quest.Chapter;
 import dev.ftb.mods.ftbquests.quest.Quest;
 import dev.ftb.mods.ftbquests.quest.TeamData;
 import dev.ftb.mods.ftbquests.quest.reward.Reward;
+import dev.ftb.mods.ftbquests.quest.reward.RewardTypes;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
@@ -24,6 +26,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
 import net.minecraft.util.Util;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +43,7 @@ public class RewardSelectorScreen extends AbstractGroupedButtonListScreen<Chapte
     private int widestQuestName;
     private int maxRewardCount;
     private int totalRewards;
+    private int xpRewards;
     private int excludedRewards;
     private int msgTimeout = 0;
     private Component message = Component.empty();
@@ -70,7 +74,7 @@ public class RewardSelectorScreen extends AbstractGroupedButtonListScreen<Chapte
         List<GroupData<Chapter,Quest>> groups = new ArrayList<>();
 
         Player player = Minecraft.getInstance().player;
-        widestQuestName = maxRewardCount = excludedRewards = totalRewards = 0;
+        widestQuestName = maxRewardCount = excludedRewards = totalRewards = xpRewards = 0;
 
         if (player != null && ClientQuestFile.exists()) {
             TeamData data = ClientQuestFile.getInstance().selfTeamData;
@@ -86,6 +90,9 @@ public class RewardSelectorScreen extends AbstractGroupedButtonListScreen<Chapte
                             if (!data.isRewardClaimed(player.getUUID(), reward)) {
                                 totalRewards++;
                                 unclaimed++;
+                                if (reward.getType() == RewardTypes.XP_LEVELS || reward.getType() == RewardTypes.XP) {
+                                    xpRewards++;
+                                }
                             }
                             if (reward.getExcludeFromClaimAll()) {
                                 excludedRewards++;
@@ -121,11 +128,14 @@ public class RewardSelectorScreen extends AbstractGroupedButtonListScreen<Chapte
         return true;
     }
 
-    private void doClaimAll() {
-        Play2ServerNetworking.send(ClaimAllRewardsMessage.INSTANCE);
-        message = excludedRewards > 0 ?
-                Component.translatable("ftbquests.gui.claim_all_exclusion", excludedRewards) :
-                Component.translatable("ftbquests.gui.all_rewards_claimed");
+    private void doClaimAll(boolean xpOnly) {
+            Play2ServerNetworking.send(new ClaimAllRewardsMessage(xpOnly));
+        message = xpOnly ?
+                Component.translatable("ftbquests.gui.xp_rewards_claimed") :
+                (excludedRewards > 0 ?
+                        Component.translatable("ftbquests.gui.claim_all_exclusion", excludedRewards) :
+                        Component.translatable("ftbquests.gui.all_rewards_claimed")
+                );
         msgTimeout = REFRESH_TIME;
     }
 
@@ -225,6 +235,7 @@ public class RewardSelectorScreen extends AbstractGroupedButtonListScreen<Chapte
 
     private class CustomBottomPanel extends Panel {
         private Button buttonClaimAll;
+        private Button buttonClaimXP;
         private final Button buttonClose;
 
         public CustomBottomPanel() {
@@ -240,11 +251,16 @@ public class RewardSelectorScreen extends AbstractGroupedButtonListScreen<Chapte
                     new Component[] { Component.translatable("ftbquests.gui.claim_all_exclusion", excludedRewards) } :
                     new Component[0];
             buttonClaimAll = SimpleTextButton.create(this, Component.translatable("ftbquests.reward.claim_all"),
-                    Icons.MONEY_BAG, mb -> doClaimAll(), tooltip);
+                    Icons.MONEY_BAG, mb -> doClaimAll(false), tooltip);
+            buttonClaimXP = SimpleTextButton.create(this, Component.translatable("ftbquests.reward.claim_xp"),
+                    ItemIcon.ofItem(Items.EXPERIENCE_BOTTLE), _ -> doClaimAll(true));
 
             add(buttonClose);
             if (totalRewards - excludedRewards > 0) {
                 add(buttonClaimAll);
+            }
+            if (xpRewards > 0) {
+                add(buttonClaimXP);
             }
         }
 
@@ -252,6 +268,7 @@ public class RewardSelectorScreen extends AbstractGroupedButtonListScreen<Chapte
         public void alignWidgets() {
             buttonClose.setPos(width - buttonClose.width - 5, 3);
             buttonClaimAll.setPos(buttonClose.posX - buttonClaimAll.width - 5, 3);
+            buttonClaimXP.setPos(buttonClose.posX - buttonClaimAll.width - buttonClaimXP.width - 10, 3);
         }
 
         @Override
--- common/src/main/java/dev/ftb/mods/ftbquests/net/ClaimAllRewardsMessage.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/net/ClaimAllRewardsMessage.java
@@ -3,17 +3,21 @@ package dev.ftb.mods.ftbquests.net;
 import dev.ftb.mods.ftblibrary.platform.network.PacketContext;
 import dev.ftb.mods.ftbquests.api.FTBQuestsAPI;
 import dev.ftb.mods.ftbquests.quest.ServerQuestFile;
+import dev.ftb.mods.ftbquests.quest.reward.Reward;
+import dev.ftb.mods.ftbquests.quest.reward.RewardTypes;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.server.level.ServerPlayer;
 
-public class ClaimAllRewardsMessage implements CustomPacketPayload {
+public record ClaimAllRewardsMessage(boolean xpOnly) implements CustomPacketPayload {
 	public static final Type<ClaimAllRewardsMessage> TYPE = new Type<>(FTBQuestsAPI.id("claim_all_rewards_message"));
 
-	public static final ClaimAllRewardsMessage INSTANCE = new ClaimAllRewardsMessage();
-
-	public static final StreamCodec<FriendlyByteBuf, ClaimAllRewardsMessage> STREAM_CODEC = StreamCodec.unit(INSTANCE);
+	public static final StreamCodec<FriendlyByteBuf, ClaimAllRewardsMessage> STREAM_CODEC = StreamCodec.composite(
+			ByteBufCodecs.BOOL, ClaimAllRewardsMessage::xpOnly,
+			ClaimAllRewardsMessage::new
+	);
 
 	@Override
 	public Type<ClaimAllRewardsMessage> type() {
@@ -21,15 +25,19 @@ public class ClaimAllRewardsMessage implements CustomPacketPayload {
 	}
 
 	public static void handle(ClaimAllRewardsMessage message, PacketContext context) {
-		ServerPlayer player = (ServerPlayer) context.player();
-		ServerQuestFile.getInstance().getTeamData(player).ifPresent(data -> {
-			data.getFile().forAllQuests(quest -> {
-				if (data.isCompleted(quest)) {
-					quest.getRewards().stream()
-							.filter(reward -> !reward.getExcludeFromClaimAll())
-							.forEach(reward -> data.claimReward(player, reward, true));
-				}
-			});
-		});
+			ServerPlayer player = (ServerPlayer) context.player();
+			ServerQuestFile.getInstance().getTeamData(player)
+					.ifPresent(data -> data.getFile().forAllQuests(quest -> {
+						if (data.isCompleted(quest)) {
+							quest.getRewards().stream()
+									.filter(message::shouldIncludeReward)
+									.forEach(reward -> data.claimReward(player, reward, true));
+						}
+					}));
+	}
+
+	private boolean shouldIncludeReward(Reward reward) {
+		return !reward.getExcludeFromClaimAll()
+				&& (!xpOnly || reward.getType() == RewardTypes.XP || reward.getType() == RewardTypes.XP_LEVELS);
 	}
 }
