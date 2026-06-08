fdece4bd0b334a999001611bc19097b6ae7d498c
Des Herriott <des.herriott@gmail.com>
2026-06-08 13:11:28 +0100

    fix: add server-side can-edit checking to three packets

--- common/src/main/java/dev/ftb/mods/ftbquests/net/CopyQuestMessage.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/net/CopyQuestMessage.java
@@ -8,6 +8,7 @@ import dev.ftb.mods.ftbquests.quest.reward.Reward;
 import dev.ftb.mods.ftbquests.quest.reward.RewardType;
 import dev.ftb.mods.ftbquests.quest.task.Task;
 import dev.ftb.mods.ftbquests.quest.task.TaskType;
+import dev.ftb.mods.ftbquests.util.NetUtils;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
@@ -37,7 +38,7 @@ public record CopyQuestMessage(long id, long chapterId, double qx, double qy, bo
     public static void handle(CopyQuestMessage message, NetworkManager.PacketContext context) {
         context.queue(() -> {
             BaseQuestFile file = ServerQuestFile.INSTANCE;
-            if (file.get(message.id) instanceof Quest toCopy && file.get(message.chapterId) instanceof Chapter chapter) {
+            if (file.get(message.id) instanceof Quest toCopy && file.get(message.chapterId) instanceof Chapter chapter && NetUtils.canEdit(context)) {
                 // deep copy of the quest
                 Quest newQuest = Objects.requireNonNull(QuestObjectBase.copy(toCopy, () -> new Quest(file.newID(), chapter)));
                 if (!message.copyDeps) {
